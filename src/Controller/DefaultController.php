a3122d501e8d92d9810fb5dadf68ac6682ff98cd
Andriy Svirin <andriy.svirin@ukrinsoft.com>
2016-06-16 02:09:21 -0700

    Use vendor in module

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -14,92 +14,92 @@ use Drupal\Core\Controller\ControllerBase;
  */
 class DefaultController extends ControllerBase {
 
-    public function processPushNotification($key = null) {
-        $notification = NULL;
+  public function processPushNotification($key = null) {
+    $notification = NULL;
 
-        // Ensure the push notification was sent to the proper URL
-        if ($key != \Drupal::config('killbill.settings')->get('listener_key')) {
-            // Log the failed attempt and bail.
-            \Drupal::logger('killbill')->warning('Incoming push notification did not contain the proper URL key.', []);
-            return;
-        }
-
-        if (!Drupal\killbill\KillbillHelpers::clientInitialize()) {
-            return;
-        }
-
-        // TODO - this will probably be json
-        $notification = "TODO";
-
-        // Bail if this is an empty or invalid notification
-        if (empty($notification)) {
-            return;
-        }
-
-        // Log the incoming push notification if enabled
-        if (\Drupal::config('killbill.settings')->get('push_logging')) {
-            \Drupal::logger('killbill')->notice('Incoming push notification: !notification', [
-                '!notification' => '<pre>' . \Drupal\Component\Utility\SafeMarkup::checkPlain(print_r($notification, TRUE)) . '</pre>'
-            ]);
-        }
-
-        \Drupal::moduleHandler()->invokeAll('killbill_process_push_notification', [
-            $notification
-        ]);
+    // Ensure the push notification was sent to the proper URL
+    if ($key != \Drupal::config('killbill.settings')->get('listener_key')) {
+      // Log the failed attempt and bail.
+      \Drupal::logger('killbill')->warning('Incoming push notification did not contain the proper URL key.', []);
+      return;
     }
 
-    public function catalog() {
-        if (!\Drupal\killbill\KillbillHelpers::clientInitialize()) {
-            $output['status'] = array(
-                '#plain_text' => t('Could not initialize the Killbill client.'),
-            );
-            return $output;
-        }
-
-        $catalog = new \Killbill_Catalog;
-        $catalog->initialize();
-
-        $header = [
-            t('Product name'),
-            t('Product type'),
-            t('Plan name'),
-            t('Phase type'),
-            t('Price in USD'),
-        ];
-        $rows = [];
-
-        $currency = 'USD';
-        foreach ($catalog->getBaseProducts() as $product) {
-            foreach ($product->plans as $plan) {
-                foreach ($plan->phases as $phase) {
-                    $rows[] = [
-                        t('@name', ['@name' => $product->name]),
-                        t('@type', [
-                            '@type' => $product->type
-                        ]),
-                        t('@plan', ['@plan' => $plan->name]),
-                        t('@phase', [
-                            '@phase' => $phase->type
-                        ]),
-                        t('@price', [
-                            '@price' => '$' . number_format($phase->prices->$currency, 2)
-                        ]),
-                    ];
-                }
-            }
-        }
-
-        $output['catalog'] = array(
-            '#type' => 'table',
-            '#header' => $header,
-            '#rows' => $rows,
-            '#empty' => $this->t('No catalog found.'),
-            '#attributes' => array(
-                'id' => 'my-module-table',
-            ),
-        );
-
-        return $output;
+    if (!Drupal\killbill\KillbillHelpers::clientInitialize()) {
+      return;
     }
+
+    // TODO - this will probably be json
+    $notification = "TODO";
+
+    // Bail if this is an empty or invalid notification
+    if (empty($notification)) {
+      return;
+    }
+
+    // Log the incoming push notification if enabled
+    if (\Drupal::config('killbill.settings')->get('push_logging')) {
+      \Drupal::logger('killbill')->notice('Incoming push notification: !notification', [
+        '!notification' => '<pre>' . \Drupal\Component\Utility\SafeMarkup::checkPlain(print_r($notification, TRUE)) . '</pre>'
+      ]);
+    }
+
+    \Drupal::moduleHandler()->invokeAll('killbill_process_push_notification', [
+      $notification
+    ]);
+  }
+
+  public function catalog() {
+    if (!\Drupal\killbill\KillbillHelpers::clientInitialize()) {
+      $output['status'] = array(
+        '#plain_text' => t('Could not initialize the Killbill client.'),
+      );
+      return $output;
+    }
+
+    $catalog = new \Killbill_Catalog;
+    $catalog->initialize();
+
+    $header = [
+      t('Product name'),
+      t('Product type'),
+      t('Plan name'),
+      t('Phase type'),
+      t('Price in USD'),
+    ];
+    $rows = [];
+
+    $currency = 'USD';
+    foreach ($catalog->getBaseProducts() as $product) {
+      foreach ($product->plans as $plan) {
+        foreach ($plan->phases as $phase) {
+          $rows[] = [
+            t('@name', ['@name' => $product->name]),
+            t('@type', [
+              '@type' => $product->type
+            ]),
+            t('@plan', ['@plan' => $plan->name]),
+            t('@phase', [
+              '@phase' => $phase->type
+            ]),
+            t('@price', [
+              '@price' => '$' . number_format($phase->prices->$currency, 2)
+            ]),
+          ];
+        }
+      }
+    }
+
+    $output['catalog'] = array(
+      '#type' => 'table',
+      '#header' => $header,
+      '#rows' => $rows,
+      '#empty' => $this->t('No catalog found.'),
+      '#attributes' => array(
+        'id' => 'my-module-table',
+      ),
+    );
+
+    return $output;
+  }
 
 }
--- src/KillbillHelpers.php
+++ src/KillbillHelpers.php
@@ -7,46 +7,75 @@ namespace Drupal\killbill;
  */
 class KillbillHelpers {
 
-    /**
-     * Retrive account.
-     * @param \Drupal\user\UserInterface $user
-     * @return type
-     */
-    public static function retrieveAccount(\Drupal\user\UserInterface $user) {
-        $killbill_account = new \Killbill_Account();
-        $killbill_account->externalKey = $user->id();
-        return $killbill_account->get();
+  /**
+   * Retrive account.
+   * @param \Drupal\user\UserInterface $user
+   * @return type
+   */
+  static function retrieveAccount(\Drupal\user\UserInterface $user) {
+    if (!self::clientInitialize()) {
+      return;
+    }
+    $killbill_account = new \Killbill_Account();
+    $killbill_account->externalKey = $user->id();
+    return $killbill_account->get();
+  }
+
+  /**
+   * Initialize client.
+   *
+   * @param type $settings
+   * @return boolean
+   */
+  static function clientInitialize($settings = NULL) {
+    // If no settings array was given, use the default account settings.
+    if (empty($settings)) {
+      $settings = array(
+        'server_url' => \Drupal::config('killbill.settings')->get('server_url'),
+        'api_user' => \Drupal::config('killbill.settings')->get('api_user'),
+        'api_password' => \Drupal::config('killbill.settings')->get('api_password'),
+      );
     }
 
-    /**
-     * Initialize client.
-     *
-     * @param type $settings
-     * @return boolean
-     */
-    public static function clientInitialize($settings = NULL) {
-        // If no settings array was given, use the default account settings.
-        if (empty($settings)) {
-            $settings = array(
-                'server_url' => \Drupal::config('killbill.settings')->get('server_url'),
-                'api_user' => \Drupal::config('killbill.settings')->get('api_user'),
-                'api_password' => \Drupal::config('killbill.settings')->get('api_password'),
-            );
-        }
+    require_once DRUPAL_ROOT . '/vendor/killbill/killbill-client-php/lib/killbill.php';
 
-        $path = libraries_get_path('killbill-client-php');
-        if ($path && file_exists(DRUPAL_ROOT . '/' . $path . '/lib/killbill.php')) {
-            require_once DRUPAL_ROOT . '/' . $path . '/lib/killbill.php';
-            \Killbill_Client::$serverUrl = $settings['server_url'];
-            \Killbill_Client::$apiUser = $settings['api_user'];
-            \Killbill_Client::$apiPassword = $settings['api_password'];
+    \Killbill_Client::$serverUrl = $settings['server_url'];
+    \Killbill_Client::$apiUser = $settings['api_user'];
+    \Killbill_Client::$apiPassword = $settings['api_password'];
 
-            \Drupal::logger('killbill')->info('Successfully registered the Killbill PHP library.', array());
-            return TRUE;
-        } else {
-            \Drupal::logger('killbill')->error('Could not find the Killbill PHP client library in libraries/killbill.', array());
-            return FALSE;
-        }
+    return TRUE;
+  }
+
+  /**
+   * Create account
+   * @global string $base_root
+   * @param \Drupal\user\UserInterface $account
+   * @return boolean
+   */
+  static function createAccount(\Drupal\user\UserInterface $account) {
+    if (!self::clientInitialize()) {
+      return FALSE;
     }
+
+    $accountData = new \Killbill_Account();
+    $accountData->externalKey = $account->id();
+    $accountData->name = $account->getUsername();
+    $accountData->email = $account->getEmail();
+    $accountData->currency = 'USD';
+    $accountData->paymentMethodId = null;
+    $accountData->address1 = null;
+    $accountData->address2 = null;
+    $accountData->company = null;
+    $accountData->state = null;
+    $accountData->country = null;
+    $accountData->phone = null;
+    $accountData->length = strlen($accountData->name);
+    if (property_exists($account, 'timezone')) {
+      $accountData->timeZone = $account->timezone;
+    }
+
+    global $base_root;
+    $accountData->create($base_root, "DRUPAL", "DRUPAL_HOOK_USER_INSERT::" . \Drupal::request()->getClientIp());
+  }
 
 }
